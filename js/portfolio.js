c2d7ff44c950fe85006e2e193accdff9ab97a4fe
Farukjosh11 <farukuoyejoshua79@gmail.com>
2021-08-21 13:41:42 +0100

    done

---
+++ js/portfolio.js
@@ -0,0 +1,21 @@
+// let name = "";
+
+// document.querySelector("#name").addEventListener("change", function (e) {
+//   console.log(e.target.value);
+// });
+
+// const submit = document.querySelector("#submit");
+
+// submit.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   const name = document.querySelector("#name");
+//   console.log(name);
+// });
+if(){
+
+}else{}
+
+while() {
+
+
+}
